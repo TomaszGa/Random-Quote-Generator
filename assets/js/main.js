f4459d2ba0c3096a43b67a7a2b67c59c2f19aa86
TomaszGa <tomasz.galina@gmail.com>
2017-05-26 01:21:17 +0200

    refactor code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,28 @@
+"use strict";
 
 $(document).ready(function () {
     
+    //clear cache so new quotes can be loaded
     $.ajaxSetup({
         cache:false
     });
     
-    $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(json){
-      var htmlString = "";
-      var linkString = "";
-      //remove unnecessary formating from the json content
-      var content = json[0].content.slice(3, -7);
-      htmlString+= "<h3><i class=\"fa fa-quote-left\" aria-hidden=\"true\"></i>" + content + "<i class=\"fa fa-quote-right\" aria-hidden=\"true\"></i></h3><p class='author'>—" + json[0].title + "</p>";
-      console.log(htmlString);
-      $(".message").html(htmlString);
-      linkString += "https://twitter.com/intent/tweet?text=" + content + " Author: " + json[0].title;
-      $("#tweetBtn").attr("href", linkString);
-    });
+    getQuote();
 
     $("#getMessage").click(function(){
-         $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(json){
-      var htmlString = "";
-      var linkString = "";
+      getQuote();      
+    });
+
+  function getQuote(){
+   $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(json){
+      let htmlString = "";
+      let linkString = "";
       //remove unnecessary formating from the json content
-      var content = json[0].content.slice(3, -7);
+      let content = json[0].content.slice(3, -7);
       htmlString+= "<h3><i class=\"fa fa-quote-left\" aria-hidden=\"true\"></i>" + content + "<i class=\"fa fa-quote-right\" aria-hidden=\"true\"></i></h3><p class='author'>—" + json[0].title + "</p>";
-      console.log(htmlString);
       $(".message").html(htmlString);
       linkString += "https://twitter.com/intent/tweet?text=" + content + " Author: " + json[0].title;
       $("#tweetBtn").attr("href", linkString);
-    });   
-        
-        
-        
-    });
+    });    
+  }
 });
-
-//htmlString += "<h3><i class=\"fa fa-quote-left\" aria-hidden=\"true\"></i>" + json[0]. + "<i class=\"fa fa-quote-right\" aria-hidden=\"true\"></i></h3>";
